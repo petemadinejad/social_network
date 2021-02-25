f4a97893d6ae2075faab90b0b20bf2c5ce01acac
parisaetemadinejad <parisaetemadinejad@gmail.com>
2021-02-26 00:05:48 +0330

    i design a simple front for site

--- social_network/settings.py
+++ social_network/settings.py
@@ -122,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# Add this new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
